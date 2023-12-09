d8ef0cf621c4f32fbc3c7cf26aab4ff7e1ace9a1
ninnim <ninnim133@gmail.com>
2023-12-09 10:42:11 +0700

    d

---
+++ docs/my-script.js
@@ -0,0 +1,27 @@
+// // alert("Test6");
+// function myFunction() {
+//     alert("Hello! I am an alert box!");
+//   };
+
+// function validateForm() {
+//   var name = document.getElementById("bankname").value;
+//   var email = document.getElementById("email").value;
+//   var phone = document.getElementById("phone").value;
+
+//   if (name == "") {
+//     alert("Please enter your name.");
+//     return false;
+//   }
+
+//   if (email == "") {
+//     alert("Please enter your email address.");
+//     return false;
+//   }
+
+//   if (phone == "") {
+//     alert("Please enter a phone number.");
+//     return false;
+//   }
+
+//   return true;
+// }
